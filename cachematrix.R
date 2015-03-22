03d51a79a9e0929a5ba5911e5e4017d9b4af0e39
Fred Smith <FredSmith1992@users.noreply.github.com>
2015-03-22 17:27:02 -0400

    extensively updated comments
    I added comments which explain how to use the script and its functions.
    (This was not at all clear from the original code base.)
    (But, then, if it had been, this exercise would have been pointless, right? :-)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 # cachematrix.R
-
-## cachematrix.R is the program we are to deliver for Programming Assignment 2 in Coursera's R Programming course.
-## It is based on a similar set of caching functions providing in rdpeng's original project,
-## which we forked from https://github.com/rdpeng/ProgrammingAssignment2 to create our own.
-
-## it serves to teach us about using functions as arguments and storing data in another context or frame
-## it has two primary functions, makeCacheMatrix() and cacheSolve(), which are used to get and set results
-
-## makeCacheMatrix holds the previously calculated value, if any, and the functions to check for, save and return it
-
-## makeCacheMatrix() is the context in which the previously calculated value, if any, and the functions to check for, save and return it
+#
+#(c)2015 FredSmith1992 (at least, on github!)
+#
+# cachematrix.R is the program we are to deliver for Programming Assignment 2 in Coursera's R Programming course.
+# It is based on a similar set of caching functions providing in rdpeng's original project,
+# which we forked from https://github.com/rdpeng/ProgrammingAssignment2 to create our own.
+#
+# The purpose of this assignment is to teach us about using functions as arguments and storing data in another context.
+#
+# Usage:
+# 1) Execute this script to create the functions and their environments
+# 2) create a matrix and store it. We will assume it is called matrixOne for purposes of this usage explanation
+#    > matrixOne<-v1<-cbind(c(1,2,3),c(23,77,91),c(15,19,53))
+# 3) set the environment  by calling makeCacheMatrix with matrixOne; I'm saving it in m1c, here:
+#    > m1c<-makeCacheMatrix(matrixOne)
+# 4) The first time you call cacheSolve() it will perform the matrix inversion and return it.
+# > cacheSolve(m1c)
+#      [,1]        [,2]        [,3]
+# [1,]  4.8  0.29795918 -1.46530612
+# [2,] -0.1  0.01632653  0.02244898
+# [3,] -0.1 -0.04489796  0.06326531
+# >
+#
+# 5) On subsequent calls to cacheSolve(), it displays "getting cached data" and returns the cached result, saving time
+# > cacheSolve(m1c)
+# getting cached data
+#      [,1]        [,2]        [,3]
+# [1,]  4.8  0.29795918 -1.46530612
+# [2,] -0.1  0.01632653  0.02244898
+# [3,] -0.1 -0.04489796  0.06326531
+# >
+# This script has two primary functions, makeCacheMatrix() and cacheSolve(), which are used to set and get results
+#
+# makeCacheMatrix holds the previously calculated value, if any, and the functions to check for, save and return it
+#
+# makeCacheMatrix() is the context in which the previously calculated value, if any, and the functions to check for, save and return it
 
 makeCacheMatrix <- function(x = matrix()) {
      m <- NULL
